66890c6f1245a63e73a44f3cb728da653155d238
Alberto <carloslopezherrera91@gmail.com>
2023-09-07 22:12:55 -0600

    Added a Swap 2 - 0.

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def neighborhood_mapping(solution, m, i_max, c_max, data, c):
         return True, solution, c, c_max, i_max
 
 
-def neighbour_evaluation(cmax, cs, i_max, i_target, task, data):
+def neighbour_evaluation_sw10(cmax, cs, i_max, i_target, task, data):
     update = False
     neighbour_c = [c for c in cs]
     neighbour_c[i_max] = cs[i_max] - get_cvalue(i_max, task, data)
@@ -108,36 +108,124 @@ def neighbour_evaluation(cmax, cs, i_max, i_target, task, data):
     return update, new_c_max
 
 
-def swap_1_0(solution, origin_i, target_i, n_j):
+def neighbour_evaluation_sw20(cmax, cs, i_max, target_1, target_2, task_1, task_2, data):
+    update = False
+    neighbour_c = [c for c in cs]
+    neighbour_c[i_max] = neighbour_c[i_max] - get_cvalue(i_max, task_1, data)
+    neighbour_c[i_max] = neighbour_c[i_max] - get_cvalue(i_max, task_2, data)
+    neighbour_c[target_1] = neighbour_c[target_1] + get_cvalue(target_1, task_1, data)
+    neighbour_c[target_2] = neighbour_c[target_2] + get_cvalue(target_2, task_2, data)
+    new_c_max, _ = fitness(neighbour_c)
+    if new_c_max < cmax:
+        update = True
+    return update, new_c_max
+
+
+def swap_1_0(solution, origin, target, task):
     swaped_solution = [m for m in solution]
-    swaped_solution[origin_i].remove(n_j)
-    swaped_solution[target_i] = swaped_solution[target_i] + [n_j]
+    swaped_solution[origin].remove(task)
+    swaped_solution[target] = swaped_solution[target] + [task]
     return swaped_solution
 
 
-def neighborhood_mapping_new(solution, m, i_max, c_max, data, c, moves, good_neighbours):
+def swap_2_0(solution, origin, target_1, target_2, task_1, task_2):
+    swaped_solution = [m for m in solution]
+    swaped_solution[origin].remove(task_1)
+    swaped_solution[target_1] = swaped_solution[target_1] + [task_1]
+    swaped_solution[origin].remove(task_2)
+    swaped_solution[target_2] = swaped_solution[target_2] + [task_2]
+    return swaped_solution
+
+
+def neighborhood_mapping_sw10(solution, m, i_max, c_max, data, c, moves, good_neighbours):
+    # Create best target machine, task and cmax variables
     best_target = 0
     best_task = 0
-    best_cmax = cmax
+    best_cmax = c_max
+    # Set found_solution to false. Only change when/if a better neighbour is found.
     found_solution = False
 
-    for y, task in enumerate(solution[i_max]):
+    # Iterate over the task of the machine with Cmax
+    for task in solution[i_max]:
+        # Iterate over all machines
         for m_i in range(m):
+            # Skip the origin machine to avoid repetitions
             if m_i != i_max:
-                update, new_cmax = neighbour_evaluation(best_cmax, c, i_max, m_i, task, data)
+                # Evaluate the neighbour
+                update, new_cmax = neighbour_evaluation_sw10(best_cmax, c, i_max, m_i, task, data)
+                print('Cmax: ', c_max, 'Best: ', best_cmax, 'Good N', good_neighbours, 'Moves: ', moves)
+                # if the neighbour is better, update the best values
                 if update:
+                    # Update the counter for improvements
                     good_neighbours += 1
+                    # Best target is the current machine
                     best_target = m_i
+                    # Best task is the current task
                     best_task = task
+                    # Best Cmax is the current Cmax
                     best_cmax = new_cmax
+                    # A better solution was found
                     found_solution = True
-
+    # If a better solution was found, create the neighbour.
     if found_solution:
+        # Update the counter of neighbour created
         moves += 1
+        # Build the neighbour
         solution = swap_1_0(solution, i_max, best_target, best_task)
+        # Get the C values
         c = generate_cvalues(m, solution, data)
+        # Get the Cmax and its index
         c_max, i_max = fitness(c)
+    # Return results
+    return found_solution, solution, c, c_max, i_max, moves, good_neighbours
 
+
+def neighborhood_mapping_sw20(solution, m, i_max, c_max, data, c, moves, good_neighbours):
+    # Create best target machines, tasks and cmax variables
+    best_target = (0, 0)
+    best_task = (0, 0)
+    best_cmax = c_max
+    # Set found_solution to false. Only change when/if a better neighbour is found.
+    found_solution = False
+
+    # Iterate over the task of the machine with Cmax for the first element
+    for x in range(len(solution[i_max])):
+        for y in range(x + 1, len(solution[i_max])):
+            # Iterate over all machines for the first target
+            for m_x in range(m):
+                # Skip the origin machine to avoid repetitions
+                if m_x != i_max:
+                    # Iterate over all machines for the second target
+                    for m_y in range(m):
+                        # Skip the origin machine to avoid repetitions
+                        if m_y != i_max:
+                            # Evaluate the neighbour
+                            update, new_cmax = neighbour_evaluation_sw20(best_cmax, c, i_max, m_x, m_y, solution[i_max][x], solution[i_max][y], data)
+                            print('Cmax: ', c_max, 'Best: ', best_cmax, 'Good N', good_neighbours, 'Moves: ', moves)
+                            # if the neighbour is better, update the best values
+                            if update:
+                                # Update the counter for improvements
+                                good_neighbours += 1
+                                # Best targets are the current machines
+                                best_target = (m_x, m_y)
+                                # Best tasks are the current task
+                                best_task = (solution[i_max][x], solution[i_max][y])
+                                # Best Cmax is the current Cmax
+                                best_cmax = new_cmax
+                                # A better solution was found
+                                found_solution = True
+    # If a better solution was found, create the neighbour.
+    if found_solution:
+        print('Found')
+        # Update the counter of neighbour created
+        moves += 1
+        # Build the neighbour
+        solution = swap_2_0(solution, i_max, best_target[0], best_target[1], best_task[0], best_task[1])
+        # Get the C values
+        c = generate_cvalues(m, solution, data)
+        # Get the Cmax and its index
+        c_max, i_max = fitness(c)
+    # Return results
     return found_solution, solution, c, c_max, i_max, moves, good_neighbours
 
 
@@ -153,10 +241,10 @@ if __name__ == '__main__':
     # Drop 'list.txt'
     all_instances.remove('list.txt')
     # For testing, only use instance 111.txt [5]
-    for instance in all_instances:
+    for instance in [all_instances[5]]:
         machines, tasks, data = get_instance(instance, folder_path)
 
-        with open(f'Results/{instance[:-4]}_R.txt', 'w') as print_results:
+        with open(f'Results/{instance[:-4]}_R-T.txt', 'w') as print_results:
             # Start the iteration process
             for iteration in range(max_iteration):
                 print(instance)
@@ -175,7 +263,7 @@ if __name__ == '__main__':
                 # Set the stuck indicator to False
                 not_stuck = True
                 while not_stuck:
-                    not_stuck, current_solution, c, cmax, i_max, total_movements, total_neighbours = neighborhood_mapping_new(
+                    not_stuck, current_solution, c, cmax, i_max, total_movements, total_neighbours = neighborhood_mapping_sw20(
                         current_solution, machines, i_max,
                         cmax,
                         data, c, total_movements, total_neighbours)
